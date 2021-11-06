26113ed9a05e545cc8ca622327411b4b74a0cd4d
Darlley <systembrtt@gmail.com>
2021-11-05 20:30:14 -0400

    header finished

--- components/Menu.js
+++ components/Menu.js
@@ -1,27 +1,33 @@
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import Image from 'next/image'
+
+import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import styles from '../styles/Menu.module.scss'
+
+import isologo from '../public/logo-1.svg'
+import isotipo from '../public/isotipo.svg'
 
 export default function Menu() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home" className="d-flex align-items-center">
+          <Image
+            width={100}
+            height={30}
+            src={isotipo}
+            alt="Isotipo"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link">Quem somos</Nav.Link>
+            <Nav.Link href="#link">Planos</Nav.Link>
+            <Nav.Link href="#link">Fale conosco</Nav.Link>
+            <div className="d-flex align-items-center ms-lg-5">
+              <Button variant="dark" className="pe-5 ps-5">Entrar</Button>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,56 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import Menu from '../components/Menu'
 
-// import styles from '../styles/Home.module.scss'
-import ilustrationHome from '../public/illustrations/Home.svg'
+import styles from '../styles/Home.module.scss'
+
+import ilustrationHome from '../public/illustrations/header.png'
+import robot from '../public/icon-robot.png'
 
 export default function Home() {
   return (
-    <div className="">
+    <>
       <Head>
         <title>Desafio | Privatecode</title>
-        <meta name="description" content="Landingpage para o desafio da privatecode" />
+        <meta
+          name="description"
+          content="Landingpage para o desafio da privatecode"
+        />
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#252160" />
       </Head>
 
-      <header>
-        <Menu />
+      <Menu />
+      <header className={styles.header}>
+
+        <div className={styles.headerContent}>
+          <div className={styles.retangule}></div>
+
+          <div>
+            <Image src={robot} width={51} height={37} />
+            <span>Seja aprovado no Enem e Vestibulares :)</span>
+          </div>
+          <h1>Você escolhe<br />o que aprender<span>.</span></h1>
+          <p>
+            Aqui você terá toda assistência queprecisa com materiais
+            desenvolvido por <strong>mestres e doutores em matemática</strong>.
+          </p>
+          <Link href="#">
+            <a className={styles.headerButtom}>
+              Veja nossos planos
+              <i className="bi bi-arrow-right"></i>
+            </a>
+          </Link>
+
+        </div>
+
+        <div className={styles.ilustrations}>
+          <Image src={ilustrationHome} width={656} height={586} />
+        </div>
+
       </header>
-    </div>
+    </>
   )
 }
